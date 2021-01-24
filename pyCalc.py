e4b4abc8e489034769e5b4e13071eef0c3e7ef8a
mulwasteveh <stephenmulwa344@gmail.com>
2021-01-24 12:08:37 +0300

    py_calculator

---
+++ pyCalc.py
@@ -0,0 +1,39 @@
+#defining an addition function
+def add(a,b):
+    return a+b
+
+#defining a substraction function
+def subtract(a,b):
+    return a-b
+
+#defining a multiplication function
+def multiply(a,b):
+    return a*b
+
+#defining a division function
+def divide(a,b):
+    return a/b
+
+print("select options:")
+print("1.Add")
+print("2.subtract")
+print("3.multiplily")
+print("4.divide")
+
+choice = input("Enter your choice(1/2/3/4):")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if choice == '1':
+    print(num1, "+", num2, "=", add(num1, num2))
+
+elif choice == '2':
+    print(num1, "-", num2, "=", subtract(num1, num2))
+
+elif choice == '3':
+    print(num1, "*", num2, "=", multiply(num1, num2))
+elif choice == '4':
+    print(num1, "/", num2, "=", divide(num1, num2))
+else:
+    print("Invalid input")
